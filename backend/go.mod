b5b2d1d2acb2d9f1db066792e8ce008154e731c8
Aditya Ghidora <adityaghidora@gmail.com>
2026-04-11 10:57:05 +0530

    feat: Connect api to database

--- backend/go.mod
+++ backend/go.mod
@@ -5,5 +5,7 @@ go 1.22.3
 require (
 	github.com/caarlos0/env/v10 v10.0.0
 	github.com/go-chi/chi/v5 v5.2.5
+	github.com/jmoiron/sqlx v1.4.0
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.12.3
 )
--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func CreateBaseRouter() *chi.Mux {
@@ -23,6 +25,15 @@ func CreateBaseRouter() *chi.Mux {
 func main() {
 	cfg := LoadConfig()
 	r := CreateBaseRouter()
+
+	log.Printf("Connecting to %s database at %s:%d...", cfg.DbName, cfg.DbHost, cfg.DbPort)
+	db, err := sqlx.Connect("postgres", cfg.getDbUrl())
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database :%v", err)
+	}
+
+	defer db.Close()
 
 	addr := fmt.Sprintf(":%d", cfg.Port)
 	log.Printf("Starting server on %d...", cfg.Port)
